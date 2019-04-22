ef5438f0699c8ba4dd32bc48e09770f24e2418ad
Michael Johnson <michael@ociaw.com>
2019-04-22 00:00:32 -0500

    Begin work on a cross-platform UI built on Avalonia

---
+++ RoverSim.AvaloniaHost/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace RoverSim.AvaloniaHost.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ RoverSim.AvaloniaHost/Views/AiListView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RoverSim.AvaloniaHost.Views
+{
+    public class AiListView : UserControl
+    {
+        public AiListView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
