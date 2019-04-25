93030a1af7295f889a1d0b70fb1279bf20ae2f0f
Michael Johnson <michael@ociaw.com>
2019-04-25 08:07:59 -0500

    Add stat collecting rover

---
+++ Core/RoverStats.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace RoverSim
+{
+    public readonly struct RoverStats
+    {
+        public RoverStats
+        (
+            Int32 movesLeft,
+            Int32 power,
+
+            Int32 samplesCollected,
+            Int32 samplesProcessed,
+            Int32 samplesTransmitted,
+
+            Int32 collectPowerCallCount,
+            Int32 powerCumulative,
+
+            Int32 collectSampleCallCount,
+            Int32 smoothSampleCumulative,
+            Int32 roughSampleCumulative,
+
+            Int32 processSamplesCallCount,
+
+            Int32 transmitCallCount,
+
+            Int32 moveCallCount,
+            Int32 moveCount,
+
+            Int32 senseCallCount
+        )
+        {
+            MovesLeft = movesLeft;
+            Power = power;
+
+            SamplesCollected = samplesCollected;
+            SamplesProcessed = samplesProcessed;
+            SamplesTransmitted = samplesTransmitted;
+
+            CollectPowerCallCount = collectPowerCallCount;
+            PowerCumulative = powerCumulative;
+
+            CollectSampleCallCount = collectSampleCallCount;
+            SmoothSampleCumulative = smoothSampleCumulative;
+            RoughSampleCumulative = roughSampleCumulative;
+
+            ProcessSamplesCallCount = processSamplesCallCount;
+
+            TransmitCallCount = transmitCallCount;
+
+            MoveCallCount = moveCallCount;
+            MoveCount = moveCount;
+
+            SenseCallCount = senseCallCount;
+        }
+
+        public Int32 MovesLeft { get; }
+
+        public Int32 Power { get; }
+
+        public Int32 SamplesCollected { get; }
+
+        public Int32 SamplesProcessed { get; }
+
+        public Int32 SamplesTransmitted { get; }
+
+
+        public Int32 CollectPowerCallCount { get; }
+        public Int32 PowerCumulative { get; }
+
+        public Int32 CollectSampleCallCount { get; }
+        public Int32 SmoothSampleCumulative { get; }
+        public Int32 RoughSampleCumulative { get; }
+
+        public Int32 ProcessSamplesCallCount { get; }
+
+        public Int32 TransmitCallCount { get; }
+
+        public Int32 MoveCallCount { get; }
+        public Int32 MoveCount { get; }
+
+        public Int32 SenseCallCount { get; }
+    }
+}
---
+++ Core/StatsRover.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace RoverSim
+{
+    public sealed class StatsRover : IRover
+    {
+        private Int32 _collectPowerCallCount;
+        private Int32 _powerCumulative;
+
+        private Int32 _collectSampleCallCount;
+        private Int32 _smoothSampleCumulative;
+        private Int32 _roughSampleCumulative;
+
+        private Int32 _processSamplesCallCount;
+
+        private Int32 _transmitCallCount;
+
+        private Int32 _moveCallCount;
+        private Int32 _moveCount;
+
+        private Int32 _senseCallCount;
+        
+        public StatsRover(IRover wrappedRover)
+        {
+            WrappedRover = wrappedRover ?? throw new ArgumentNullException(nameof(wrappedRover));
+        }
+
+        private IRover WrappedRover { get; }
+
+        public Int32 MovesLeft => WrappedRover.MovesLeft;
+
+        public Int32 Power => WrappedRover.Power;
+
+        public Int32 SamplesCollected => WrappedRover.SamplesCollected;
+
+        public Int32 SamplesProcessed => WrappedRover.SamplesProcessed;
+
+        public Int32 SamplesTransmitted => WrappedRover.SamplesTransmitted;
+
+        public Int32 NoBacktrack => WrappedRover.NoBacktrack;
+
+        public Boolean IsHalted => WrappedRover.IsHalted;
+
+        public Int32 PosX => WrappedRover.PosY;
+
+        public Int32 PosY => WrappedRover.PosX;
+
+        public RoverStats GetStats() => new RoverStats(
+            MovesLeft,
+            Power,
+
+            SamplesCollected,
+            SamplesProcessed,
+            SamplesTransmitted,
+
+            _collectPowerCallCount,
+            _powerCumulative,
+
+            _collectSampleCallCount,
+            _smoothSampleCumulative,
+            _roughSampleCumulative,
+
+            _processSamplesCallCount,
+
+            _transmitCallCount,
+
+            _moveCallCount,
+            _moveCount,
+
+            _senseCallCount
+        );
+
+        public Int32 CollectPower()
+        {
+            _collectPowerCallCount++;
+            Int32 powerAmount = WrappedRover.CollectPower();
+            _powerCumulative += powerAmount;
+            return powerAmount;
+        }
+
+        public (Boolean isSuccess, TerrainType newTerrain) CollectSample()
+        {
+            _collectSampleCallCount++;
+            (Boolean isSuccess, TerrainType newTerrain) = WrappedRover.CollectSample();
+            if (isSuccess)
+            {
+                if (newTerrain == TerrainType.SampledSmooth)
+                    _smoothSampleCumulative++;
+                else
+                    _roughSampleCumulative++;
+            }
+            return (isSuccess, newTerrain);
+        }
+
+        public Int32 ProcessSamples()
+        {
+            _processSamplesCallCount++;
+            Int32 processCount = WrappedRover.ProcessSamples();
+            return processCount;
+        }
+
+        public Int32 Transmit()
+        {
+            _transmitCallCount++;
+            Int32 transmitCount = WrappedRover.Transmit();
+            return transmitCount;
+        }
+
+        public Boolean Move(Direction direction)
+        {
+            _moveCallCount++;
+            Boolean success = WrappedRover.Move(direction);
+            if (success)
+                _moveCount++;
+            return success;
+        }
+
+        public TerrainType SenseSquare(Direction direction)
+        {
+            _senseCallCount++;
+            TerrainType terrain = WrappedRover.SenseSquare(direction);
+            return terrain;
+        }
+    }
+}
