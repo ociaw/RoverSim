3d22f43f4ee35fd6bfc6eae52e97870ed62b9c56
Michael Johnson <michael@ociaw.com>
2019-08-30 12:14:12 -0500

    Reduce memory usage and sim time by processing sims as they complete instead of all at once at the end

--- WinFormsClient/WorkForm.cs
+++ WinFormsClient/WorkForm.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
-using System.IO;
-using CsvHelper;
 
 namespace RoverSim.WinFormsClient
 {
@@ -42,111 +40,28 @@ namespace RoverSim.WinFormsClient
             List<IAiFactory> selectedAis = aiFactories.Where(t => AiList.SelectedItems.ContainsKey(t.Name)).ToList();
             TimeUsed.Text = "Working...";
             var stopwatch = Stopwatch.StartNew();
-            var results = await manager.Simulate(selectedAis, runCount);
+            (var results, var worstSim, var worstAi) = await manager.Simulate(selectedAis, runCount);
             stopwatch.Stop();
             TimeUsed.Text = stopwatch.Elapsed.TotalSeconds.ToString();
 
-            (_renderSim, _renderAiFactory) = FindWorstSim(results);
+            _renderSim = worstSim;
+            _renderAiFactory = worstAi;
             if (_renderSim != null && _renderSim.HasError)
                 MessageBox.Show("ERROR");
 
-            SaveResults(results);
-            running = false;
-        }
-
-        private static (CompletedSimulation worstSim, IAiFactory worstAi) FindWorstSim(Dictionary<IAiFactory, List<CompletedSimulation>> results)
-        {
-            CompletedSimulation overallWorst = null;
-            IAiFactory worstAi = null;
-
-            foreach (var kvp in results)
+            foreach (var aiFactory in results.Keys)
             {
-                var errored = kvp.Value.FirstOrDefault(sim => sim.HasError);
-                if (errored != null)
-                {
-                    return (errored, kvp.Key);
-                }
-
-                CompletedSimulation worst = kvp.Value.OrderBy(sim => sim.Stats.SamplesTransmitted).FirstOrDefault();
-                if (overallWorst == null || worst != null && worst.Stats.SamplesTransmitted < overallWorst.Stats.SamplesTransmitted)
-                {
-                    overallWorst = worst;
-                    worstAi = kvp.Key;
-                }
-            }
-
-            return (overallWorst, worstAi);
-        }
-
-        private void SaveResults(Dictionary<IAiFactory, List<CompletedSimulation>> results)
-        {
-            var aggregates = new Dictionary<String, (Double meanMoves, Double meanPower, Double meanSamples, Double sampleVariance)>();
-
-            String documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            TextWriter writer = new StreamWriter(documentsFolder + "\\MarsRoverScratch.csv");
-            using (CsvWriter csv = new CsvWriter(writer))
-            {
-                csv.WriteField("AI Type");
-                csv.WriteField("Moves Left");
-                csv.WriteField("Power Left");
-                csv.WriteField("Samples Collected");
-                csv.WriteField("Samples Processed");
-                csv.WriteField("Samples Transmitted");
-                csv.NextRecord();
-
-                foreach (var kvp in results)
-                {
-                    String aiName = kvp.Key.Name;
-                    Int64 moves = 0;
-                    Int64 power = 0;
-                    Int64 samples = 0;
-
-                    // Used for calculating the standard deviation
-                    Int32 count = 0;
-                    Double meanSamples = 0;
-                    Double mean2Samples = 0;
-
-                    foreach (var sim in kvp.Value)
-                    {
-                        RoverStats stats = sim.Stats;
-
-                        csv.WriteField(kvp.Key.Name);
-                        csv.WriteField(stats.MovesLeft.ToString());
-                        csv.WriteField(stats.Power.ToString());
-                        csv.WriteField(stats.SamplesCollected.ToString());
-                        csv.WriteField(stats.SamplesProcessed.ToString());
-                        csv.WriteField(stats.SamplesTransmitted.ToString());
-                        csv.NextRecord();
-
-                        moves += stats.MovesLeft;
-                        power += stats.Power;
-                        samples += stats.SamplesTransmitted;
-
-                        count++;
-                        Double delta = stats.SamplesTransmitted - meanSamples;
-                        meanSamples += delta / count;
-                        Double delta2 = stats.SamplesTransmitted - meanSamples;
-                        mean2Samples += delta * delta2;
-                    }
-
-                    Double sampleVariance = mean2Samples / (count - 1);
-                    Double sampleStdDev = Math.Sqrt(sampleVariance);
-
-                    aggregates[aiName] = (moves / (Double)count, power / (Double)count, meanSamples, sampleStdDev);
-                }
-            }
-
-            foreach (KeyValuePair<String, (Double meanMoves, Double meanPower, Double meanSamples, Double sampleStdDev)> stat in aggregates)
-            {
+                var stat = results[aiFactory];
                 if (Results.Rows.Count == 0)
                 {
                     DataGridViewRow row = new DataGridViewRow();
                     Results.Rows.Add(row);
                 }
 
-                (var meanMoves, var meanPower, var meanSamples, var sampleStdDev) = stat.Value;
-                Results.Rows[0].SetValues(stat.Key, meanMoves.ToString("F2"), meanPower.ToString("F2"), meanSamples.ToString("F2"), sampleStdDev.ToString("F2"));
+                (var meanMoves, var meanPower, var meanSamples, var sampleStdDev) = stat;
+                Results.Rows[0].SetValues(aiFactory.Name, meanMoves.ToString("F2"), meanPower.ToString("F2"), meanSamples.ToString("F2"), sampleStdDev.ToString("F2"));
             }
+            running = false;
         }
 
         private void OpenRender_Click(object sender, EventArgs e)
--- WinFormsClient/WorkManager.cs
+++ WinFormsClient/WorkManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using CsvHelper;
 using RoverSim.Ais;
 using RoverSim.ScratchAis;
 
@@ -8,10 +10,13 @@ namespace RoverSim.WinFormsClient
 {
     internal class WorkManager
     {
-        internal async Task<Dictionary<IAiFactory, List<CompletedSimulation>>> Simulate(IList<IAiFactory> aiFactories, Int32 runCount)
+        public String OutputDirectory { get; } = Directory.GetCurrentDirectory();
+
+        internal async Task<(Dictionary<IAiFactory, (Double meanMoves, Double meanPower, Double meanSamples, Double sampleStdDev)> aggregates, CompletedSimulation worstSim, IAiFactory worstAi)> Simulate(IList<IAiFactory> aiFactories, Int32 runCount)
         {
-            var tasks = new List<Task>(aiFactories.Count);
-            var results = new Dictionary<IAiFactory, List<CompletedSimulation>>(aiFactories.Count);
+            var aggregates = new Dictionary<IAiFactory, (Double meanMoves, Double meanPower, Double meanSamples, Double sampleStdDev)>();
+            CompletedSimulation worstSim = null;
+            IAiFactory worstAi = null;
             Int32 levelSeed = Rando.Next(Int32.MinValue, Int32.MaxValue);
             foreach (var aiFactory in aiFactories)
             {
@@ -20,12 +25,17 @@ namespace RoverSim.WinFormsClient
                 var roverFactory = new DefaultRoverFactory();
                 var simulator = new Simulator(levelGenerator, roverFactory, aiFactory);
 
-                results[aiFactory] = new List<CompletedSimulation>(runCount);
-                tasks.Add(simulator.SimulateAsync(runCount, sim => results[aiFactory].Add(sim)));
+                using (Completer completer = Completer.Create(Path.Combine(OutputDirectory, $"RoverSim-{aiFactory.Name}.csv")))
+                {
+                    await simulator.SimulateAsync(runCount, completer.Consume);
+                    aggregates[aiFactory] = completer.GetAggregates();
+                    worstSim = Completer.ChooseWorst(worstSim, completer.WorstSim);
+                    if (worstSim == completer.WorstSim)
+                        worstAi = aiFactory;
+                }
             }
 
-            await Task.WhenAll(tasks);
-            return results;
+            return (aggregates, worstSim, worstAi);
         }
         
         public static IEnumerable<IAiFactory> GetAIs()
@@ -37,5 +47,90 @@ namespace RoverSim.WinFormsClient
                 new MarkIIFactory(),
                 new FixedStateAiFactory(),
             };
+
+        private sealed class Completer : IDisposable
+        {
+            private readonly CsvWriter _csv;
+            private Int64 _moves = 0;
+            private Int64 _power = 0;
+
+            // Used for calculating the standard deviation
+            private Int32 _count = 0;
+            private Double _meanSamples = 0;
+            private Double _mean2Samples = 0;
+
+            private Completer(CsvWriter csvWriter)
+            {
+                _csv = csvWriter ?? throw new ArgumentNullException(nameof(csvWriter));
+            }
+
+            public CompletedSimulation WorstSim { get; private set; }
+
+            public static Completer Create(String path)
+            {
+                TextWriter writer = new StreamWriter(path);
+                CsvWriter csv = new CsvWriter(writer);
+                csv.WriteField("Moves Left");
+                csv.WriteField("Power Left");
+                csv.WriteField("Samples Collected");
+                csv.WriteField("Samples Processed");
+                csv.WriteField("Samples Transmitted");
+                csv.NextRecord();
+                return new Completer(csv);
+            }
+
+            public (Double meanMoves, Double meanPower, Double meanSamples, Double sampleStdDev) GetAggregates()
+            {
+                Double sampleVariance = _mean2Samples / (_count - 1);
+                Double sampleStdDev = Math.Sqrt(sampleVariance);
+
+                return (_moves / (Double)_count, _power / (Double)_count, _meanSamples, sampleStdDev);
+            }
+
+            public void Consume(CompletedSimulation sim)
+            {
+                RoverStats stats = sim.Stats;
+
+                // Write CSV
+                _csv.WriteField(stats.MovesLeft.ToString());
+                _csv.WriteField(stats.Power.ToString());
+                _csv.WriteField(stats.SamplesCollected.ToString());
+                _csv.WriteField(stats.SamplesProcessed.ToString());
+                _csv.WriteField(stats.SamplesTransmitted.ToString());
+                _csv.NextRecord();
+
+                // Update stats
+                _moves += stats.MovesLeft;
+                _power += stats.Power;
+
+                _count++;
+                Double delta = stats.SamplesTransmitted - _meanSamples;
+                _meanSamples += delta / _count;
+                Double delta2 = stats.SamplesTransmitted - _meanSamples;
+                _mean2Samples += delta * delta2;
+
+                // Update Worst Sim
+                WorstSim = ChooseWorst(WorstSim, sim);
+            }
+
+            public void Dispose() => _csv.Dispose();
+
+            public static CompletedSimulation ChooseWorst(CompletedSimulation first, CompletedSimulation second)
+            {
+                if (first == null)
+                    return second;
+                if (second == null)
+                    return first;
+                if (first.HasError)
+                    return first;
+                if (second.HasError)
+                    return second;
+
+                if (first.Stats.SamplesTransmitted <= second.Stats.SamplesTransmitted)
+                    return first;
+
+                return second;
+            }
+        }
     }
 }
