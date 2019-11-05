bf82687f1655e36411f07500bbff2466ec0a2012
Michael Johnson <michael@ociaw.com>
2019-11-04 23:14:31 -0600

    Allow the amount of memory in the Fixed State AI to be configurable

--- Ais/FixedStateAiFactory.cs
+++ Ais/FixedStateAiFactory.cs
@@ -4,8 +4,22 @@ namespace RoverSim.Ais
 {
     public sealed class FixedStateAiFactory : IAiFactory
     {
+        public FixedStateAiFactory() 
+            : this(5) 
+        { }
+
+        public FixedStateAiFactory(Int32 memoryCount)
+        {
+            if (memoryCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(memoryCount), "Must be non-negative.");
+
+            MemoryCount = memoryCount;
+        }
+
         public String Name => "Fixed State AI";
 
-        public IAi Create(SimulationParameters parameters) => new FixedStateAi(parameters, 5);
+        public Int32 MemoryCount { get; }
+
+        public IAi Create(SimulationParameters parameters) => new FixedStateAi(parameters, MemoryCount);
     }
 }
