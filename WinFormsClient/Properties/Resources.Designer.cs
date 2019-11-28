fd56c47ce560a859a77893364364a516a2f27cee
Michael Johnson <michael@ociaw.com>
2019-11-28 00:41:26 -0600

    Standardize assembly name

--- WinFormsClient/Properties/Resources.Designer.cs
+++ WinFormsClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsClient.Properties {
+namespace RoverSim.WinFormsClient.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormsClient.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsClient.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RoverSim.WinFormsClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
